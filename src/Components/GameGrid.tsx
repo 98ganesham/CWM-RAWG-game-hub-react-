49e12c8d320d3e9518dde01854269e21d0ef62f3
98Ganesham <98ganesham@gmail.com>
2023-06-26 15:48:33 +0630

    useGames

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -1,27 +1,8 @@
-import React, { useState, useEffect } from "react";
-import apiClent from "../Services/api-clent";
+import useGames from "../hooks/useGames";
 import { Text } from "@chakra-ui/react";
 
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  counter: number;
-  results: Game[];
-}
-
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  useEffect(() => {
-    apiClent
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
-
+  const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
