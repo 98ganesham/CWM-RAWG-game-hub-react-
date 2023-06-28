60eb559e1fd77021666471c081ea39c1f549128f
98Ganesham <98ganesham@gmail.com>
2023-06-28 09:45:31 +0630

    Cleaning Up The Genres

--- game-hub/src/Components/GenreList.tsx
+++ game-hub/src/Components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -21,6 +22,8 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   if (isLoading) return <Spinner />;
 
   return (
+  <>
+  <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
     <List>
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
@@ -28,9 +31,11 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
             <Image
               boxSize="32px"
               borderRadius={8}
+              objectFit='cover'
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Button
+            <Button whiteSpace={'normal'}
+            textAlign='left'
               fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectGenre(genre)}
               fontSize="lg"
@@ -41,7 +46,9 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
           </HStack>
         </ListItem>
       ))}
-    </List>
+      </List>
+
+    </>
   );
 };
 
