03ce35119f29df1b4cacf0030d9281d75e059702
98Ganesham <98ganesham@gmail.com>
2023-07-03 14:57:39 +0630

    Fetching Platforms

--- game-hub/src/Components/PlatformSelector.tsx
+++ game-hub/src/Components/PlatformSelector.tsx
@@ -17,7 +17,7 @@ const PlatformSelector = ({ onSelectPlatform, selectPlatform }: Props) => {
         {selectPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             onClick={() => onSelectPlatform(platform)}
             key={platform.id}
--- game-hub/src/hooks/usePlatforms.ts
+++ game-hub/src/hooks/usePlatforms.ts
@@ -1,12 +1,30 @@
-import useData from './useData';
+import { useQuery } from '@tanstack/react-query';
+import PlatformSelector from '../Components/PlatformSelector';
+import useData, { FetchResponse } from './useData';
+import apiClient from '../Services/api-client';
+import platforms from '../Data/platforms';
 
-interface Platform{
+
+export interface Platform{
     id: number;
     name: string;
     slug: string;
 
 }
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+
+const usePlatforms = () =>
+useQuery({
+    queryKey: ['platforms'],
+    queryFn: () =>
+    apiClient
+    .get<FetchResponse<Platform>>('/platforms/lists/parents')
+    .then(res => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: {count: platforms.length, results: platforms},
+
+});
+    
+
 
 
 export default usePlatforms;
