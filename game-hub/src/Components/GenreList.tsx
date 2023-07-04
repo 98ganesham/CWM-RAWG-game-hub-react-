40ea7adcebfb99e63f65835aa9385cab4ebe9800
98Ganesham <98ganesham@gmail.com>
2023-07-04 15:43:19 +0630

    Simplifying Query Objects

--- game-hub/src/Components/GenreList.tsx
+++ game-hub/src/Components/GenreList.tsx
@@ -8,15 +8,16 @@ import {
   Spinner,
   Text,
 } from "@chakra-ui/react";
-import useGenres, { Genre } from "../hooks/useGenres";
+import useGenres   from "../hooks/useGenres";
 import getCroppedImageUrl from "../Services/image-url";
+import Genre from "../Entities/Genre";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null;
+  selectedGenreId?:number;
 }
 
-const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenreId, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (error) return null;
   if (isLoading) return <Spinner />;
@@ -25,7 +26,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   <>
   <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
     <List>
-      {data.map((genre) => (
+      {data?.results.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
             <Image
@@ -36,7 +37,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
             />
             <Button whiteSpace={'normal'}
             textAlign='left'
-              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              fontWeight={selectedGenreId ? "bold" : "normal"}
               onClick={() => onSelectGenre(genre)}
               fontSize="lg"
               variant="link"
